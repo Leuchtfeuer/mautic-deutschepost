6a93a3b7e76d4a2a33cb73cd1d8eb0106464c262
Florian Wessels <f.wessels@bitmotion.de>
2019-11-19 13:31:43 +0100

    [FOLLOW-UP] Generate MAS Client ID on installation

---
+++ Generator/ClientIdGenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MauticPlugin\MauticTriggerdialogBundle\Generator;
+
+
+class ClientIdGenerator
+{
+    public static function generateClientId($bytes = 3)
+    {
+        return strtoupper(bin2hex(openssl_random_pseudo_bytes($bytes)));
+    }
+}
