77cc647625b2669306920ecea81e717e904f6271
Florian Wessels <f.wessels@bitmotion.de>
2019-12-14 15:56:20 +0800

    [BUGFIX] Evaluate user permissions
    The role permissions are now evaluated.
    
    Resolves #2

---
+++ Security/Permissions/TriggerdialogPermissions.php
@@ -0,0 +1,39 @@
+<?php
+namespace MauticPlugin\MauticTriggerdialogBundle\Security\Permissions;
+
+use Mautic\CoreBundle\Security\Permissions\AbstractPermissions;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TriggerdialogPermissions extends AbstractPermissions
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($params)
+    {
+        parent::__construct($params);
+
+        $this->addStandardPermissions(['campaigns'], true);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return string|void
+     */
+    public function getName()
+    {
+        return 'triggerdialog';
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface &$builder, array $options, array $data)
+    {
+        $this->addStandardFormFields($this->getName(), 'campaigns', $builder, $data);
+    }
+}
