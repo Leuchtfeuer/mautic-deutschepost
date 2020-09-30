9c26001fd6d04c84f7a094443fef28a6ec6e7f0e
Lukas Guenther <l.guenther@leuchtfeuer.com>
2020-09-30 11:50:56 +0200

    [bugfix] changed to new api

--- Config/config.php
+++ Config/config.php
@@ -12,7 +12,7 @@ use MauticPlugin\MauticTriggerdialogBundle\Model\TriggerCampaignModel;
 return [
     'name' => 'Dt. Post',
     'description' => 'Send postcards or letters via Deutsche Post TRIGGERDIALOG',
-    'version' => '1.0.2',
+    'version' => '1.1.0',
     'author' => 'Florian Wessels',
 
     'menu' => [
