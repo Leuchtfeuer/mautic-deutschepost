a6c9e1c16fd1650d562be9511705b0986b247cef
Lukas Guenther <l.guenther@leuchtfeuer.com>
2020-11-11 07:20:20 +0100

    [bugfix] changed to new major version

--- Config/config.php
+++ Config/config.php
@@ -13,7 +13,7 @@ use MauticPlugin\MauticTriggerdialogBundle\Utility\SsoUtility;
 return [
     'name' => 'Dt. Post',
     'description' => 'Send postcards or letters via Deutsche Post TRIGGERDIALOG',
-    'version' => '1.2.0',
+    'version' => '2.0.0',
     'author' => 'Florian Wessels',
 
     'menu' => [
