4ba99b89a4c045b9bfdddbfce31683e76fe1708e
Lukas Guenther <l.guenther@leuchtfeuer.com>
2020-11-11 07:19:52 +0100

    [bugfix] removed unused variable

--- Controller/TriggerCampaignController.php
+++ Controller/TriggerCampaignController.php
@@ -605,11 +605,11 @@ class TriggerCampaignController extends AbstractFormController
             //lock the entity
             $triggerCampaignModel->lockEntity($triggerCampaign);
         }
-        $test = $this->setFormTheme($form, self::TEMPLATES['form'], self::THEMES['variables']);
+
 
         return $this->delegateView([
             'viewParameters' => [
-                'form' => $test,
+                'form' => $this->setFormTheme($form, self::TEMPLATES['form'], self::THEMES['variables']),
                 'currentListId' => $triggerCampaign->getId(),
             ],
             'contentTemplate' => self::TEMPLATES['form'],
