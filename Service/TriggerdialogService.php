1bb9a84992ad8141ef683a1767f2228bbe206b93
Florian Wessels <flo.wessels@gmail.com>
2020-11-11 16:39:43 +0100

    [TASK] Use de as default locale

--- Service/TriggerdialogService.php
+++ Service/TriggerdialogService.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class TriggerdialogService
 {
-    const LOCALE = 'en';
+    const LOCALE = 'de';
 
     const SESSION_KEY = 'triggerdialog';
 
