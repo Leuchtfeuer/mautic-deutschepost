b141caa9cebf4d6523531185dd834dcec7d8f791
Adrian Schimpf <adrian@idea2.ch>
2021-10-27 11:33:49 +0200

    add intro texts to settings page

--- Views/FormTheme/Config/_config_triggerdialogconfig_widget.html.php
+++ Views/FormTheme/Config/_config_triggerdialogconfig_widget.html.php
@@ -8,6 +8,25 @@
 use Mautic\CoreBundle\Templating\Engine\PhpEngine;
 use Symfony\Component\Form\FormView;
 
+$fields    = $form->children;
+// print '<pre>';
+// error_log ('<h1>$this->coreParametersHelper->get()</h1>');
+// error_log( $this->coreParametersHelper->get('triggerdialog_masSecret') );
+// print '</pre>'; 
+
+$arrMsg[] = "Guten Tag";
+$arrMsg[] = "";
+$arrMsg[] = "Ich interessiere mich für die Nutzung von Ihrer PrintMailing Lösung mit dem Marketing Automation Tool Aivie.";
+$arrMsg[] = "";
+$arrMsg[] = "Bitte senden Sie mir einen entsprechenden Vertrag.";
+$arrMsg[] = "";
+$arrMsg[] = sprintf("MAS ID [partnerSystemIdExt]: %s",$_ENV["MAUTIC_TRIGGERDIALOG_MASID"]);
+$arrMsg[] = sprintf("MAS Client-ID [Mandanten-ID]: %s", $_ENV["MAUTIC_TRIGGERDIALOG_MASCLIENTID"]);
+$arrMsg[] = "";
+$arrMsg[] = "Vielen Dank.";
+$arrMsg[] = "";
+$arrMsg[] = "Mit freundlichen Grüssen";
+$message = implode($arrMsg,'\\r\\n');
 ?>
 
 <div class="panel panel-primary">
@@ -16,17 +35,24 @@ use Symfony\Component\Form\FormView;
 	</div>
 	<div class="panel-body">
 		<div class="row">
+			<div class="col-md-6 form-group">
+				<p>Um individuelle Print Mailings aus Kampagnen zu versenden benötigst du einen Vertrag mit der Deutschen Post.</p>
+				<p>Weitere Details und eine <a href="https://aivie.ch/postkarten-versenden-mit-mautic/" target="_blank">Schritt für Schritt Anleitung</a> findest du im Blog.</p>
+				<p><a href="#" class="btn btn-default btn-save btn-copy triggerdialog_mailto">Vertrag anfordern</a></p>
+			</div>
+		</div>	
+		<div class="row">
 			<div class="col-md-6">
-                <?php echo $view['form']->row($form->children['triggerdialog_masId']); ?>
+                <?php echo $view['form']->rowIfExists($fields, 'triggerdialog_masId'); ?>
 			</div>
 			<div class="col-md-6">
-                <?php echo $view['form']->row($form->children['triggerdialog_masClientId']); ?>
+                <?php echo $view['form']->rowIfExists($fields, 'triggerdialog_masClientId'); ?>
 			</div>
 		</div>
 		<hr />
 		<div class="row">
 			<div class="col-md-12">
-                <?php echo $view['form']->row($form->children['triggerdialog_masSecret']); ?>
+                <?php echo $view['form']->rowIfExists($fields, 'triggerdialog_masSecret'); ?>
 			</div>
 		</div>
 	</div>
@@ -39,11 +65,24 @@ use Symfony\Component\Form\FormView;
 	<div class="panel-body">
 		<div class="row">
 			<div class="col-md-6">
-                <?php echo $view['form']->row($form->children['triggerdialog_rest_user']); ?>
+                <?php echo $view['form']->rowIfExists($fields, 'triggerdialog_rest_user'); ?>
 			</div>
 			<div class="col-md-6">
-                <?php echo $view['form']->row($form->children['triggerdialog_rest_password']); ?>
+                <?php echo $view['form']->rowIfExists($fields, 'triggerdialog_rest_password'); ?>
 			</div>
 		</div>
 	</div>
 </div>
+
+<script>
+  mQuery("a.triggerdialog_mailto").click(
+      function(event) {
+		event.preventDefault();
+        
+		var email = 'print-mailing@deutschepost.de';
+    	var subject = 'Neuer Vertrag PrintMailing: <?php echo $_ENV["MAUTIC_TRIGGERDIALOG_MASID"] ?>';
+    	var emailBody = encodeURIComponent('<?php echo $message; ?>');
+    	window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+      }
+    );
+  </script>
