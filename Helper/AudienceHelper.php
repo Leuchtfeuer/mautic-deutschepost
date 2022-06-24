a710c354c01fbedcbd8e2996c92aa4f0e071daff
Lukas Günther <lukas.guenther@pm.me>
2022-06-24 09:41:34 +0200

    added mailing test frontend

--- Helper/AudienceHelper.php
+++ Helper/AudienceHelper.php
@@ -9,6 +9,8 @@ class AudienceHelper
     private const TEST_AUDIENCE = 'https://uat.print-mailing-api-test.deutschepost.de';
 
     private const FRONTEND = 'https://print-mailing.deutschepost.de';
+    
+    private const TEST_FRONTEND = 'https://uat.print-mailing-test.deutschepost.de';
 
     public static function getAudience(): string
     {
@@ -17,6 +19,6 @@ class AudienceHelper
 
     public static function getFrontend(): string
     {
-        return self::FRONTEND;
+        return MAUTIC_ENV === 'prod' ? self::FRONTEND : self::TEST_FRONTEND;
     }
 }
